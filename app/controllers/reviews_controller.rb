e97cb3a8400043a826d7ae4cb76c6bd2661afc9f
Mateo Blanc <mateoblanc93@gmail.com>
2020-06-03 16:03:18 +0200

    added link to js in stylesheet

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -18,7 +18,7 @@ class ReviewsController < ApplicationController
 
   def destroy
     review = Review.find(params[:id])
-    review.destroy!update
+    review.destroy
     redirect_to restaurant_path(review.restaurant)
   end
 
